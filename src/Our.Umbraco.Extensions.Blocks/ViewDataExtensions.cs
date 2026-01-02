2f13476caf5d12c71b2958adae203ce1394d39ae
Callum Whyte <hey@callumwhyte.com>
2026-01-02 12:04:42 +0000

    HTML helper for rendering elements with view model

---
+++ src/Our.Umbraco.Extensions.Blocks/ViewDataExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Umbraco.Cms.Core.Models.PublishedContent;
+
+namespace Our.Umbraco.Extensions.Blocks
+{
+    public static class ViewDataExtensions
+    {
+        public static IPublishedElement? GetSettings<T>(this ViewDataDictionary<T> viewData)
+            where T : IPublishedElement
+        {
+            viewData.TryGetValue("Settings", out var settings);
+
+            return settings as IPublishedElement;
+        }
+    }
+}
